abed72883e23b2d0faa44c08d38271a40366bc9c
pcherian10 <pcherian10@gmail.com>
2019-03-02 15:58:04 -0500

    performed successful storage of user from auth/google/callback

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,18 @@
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys');
 
+const User = mongoose.model('users'); // one argument to retreive, two to load.
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, 
+    }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refreshtoken', refreshToken);
-        console.log('profile', profile);
-    })
+        new User({ googleId: profile.id }).save();
+        }
+    )
 );
