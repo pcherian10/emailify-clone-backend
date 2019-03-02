5b966ba894c3259595ec7fc222bd2740edb979c4
pcherian10 <pcherian10@gmail.com>
2019-03-02 13:15:01 -0500

    successful setup of google oath token with route set up for callback

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ passport.use(new GoogleStrategy({
 })
 );
 
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+})
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
