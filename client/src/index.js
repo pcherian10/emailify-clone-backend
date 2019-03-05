f587242de367127c1a50ca75b87cab4cd314f15a
pcherian10 <pcherian10@gmail.com>
2019-03-05 16:05:45 -0500

    added Provider in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
-    <App />, 
-    document.querySelector('#root') 
+    <Provider
+        store={store}
+    >
+        <App />
+    </Provider>,
+        document.querySelector('#root') 
     );
