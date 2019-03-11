d1d8c0eecfb22a5fceac4d92811a6466958fc57a
pcherian10 <pcherian10@gmail.com>
2019-03-11 08:51:24 -0400

    updated passport google strategy for Oauth, whitelisted current ip

--- services/passport.js
+++ services/passport.js
@@ -3,8 +3,11 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose')
 const keys = require('../config/keys');
-
 const User = mongoose.model('users'); // one argument to retreive, two to load.
+
+// mongoose.connect("process.env.MONGO_URI", { useNewUrlParser: true })
+//     .then(() => console.log('Connecting to mongodb'))
+//     .catch((error) => console.log(error))
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -20,9 +23,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback', 
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    proxy: true
+    callbackURL: '/auth/google/callback'
     }, 
     async (accessToken, refreshToken, profile, done) => {
         const existingUser = await User.findOne({googleId: profile.id});
